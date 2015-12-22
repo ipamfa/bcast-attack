0e287cc9b1927ad7dacf81e3fff1f68d5ff0b560
Elmaira <ipam.fuadina@gmail.com>
2015-12-22 07:15:02 +0700

    penambahan testsuite

---
+++ testsuite_1.py
@@ -0,0 +1,42 @@
+import unittest
+from L3 import *
+
+# Test Gram-schmidt, ambil dari Bremner      
+class GramSTest(unittest.TestCase) :
+    def test_v1equal_bstar1(self):
+        X = np.array([
+            [3, -1, 5],
+            [-5, 2, -1],
+            [-3, 9, 2]
+        ])
+        gram(X)
+        #
+        y = getBstar(0)
+        x = X[0]
+        self.assertEqual( all( x==y ), True)
+        
+    def test_bstar_before_run_isNone(self) :
+        # test for 3 dim
+        self.assertIsNone( getBstar(0) )
+        self.assertIsNone( getBstar(1) )
+        self.assertIsNone( getBstar(2) )
+        
+    def test_lll_fullrank_3dim(self) :
+        self.assertEqual(1, 1)
+        
+Y = np.array([
+    [-11,  12,  -4],
+    [3, -1, 5],
+    [-5, 2, -1],
+    [-3, 9, 2]
+])
+Z = np.array([
+    [13, 15, -5,  8],
+    [8, 6, 2, 6],
+    [2, 4, 8, 4],
+    [1, -1, -6, -8],
+    [2, 6, -9, 6],
+])
+
+if __name__ == "__main__" :
+    unittest.main()
