36a0b31bc18d2a0fb154b2237459c44719219a1a
Elmaira <ipam.fuadina@gmail.com>
2016-01-02 15:22:41 +0700

    test exercise lin5a p.12

--- testsuite_0.py
+++ testsuite_0.py
@@ -16,7 +16,7 @@ class GaussElimTest(unittest.TestCase) :
             [10,11,12]
         ])
         print "kasus unik normal"
-        print solve_linear( (X.astype(float)).T, Y)
+        print solve_linear( (X.astype(float)).T, Y.astype(float) )
 
     def test_gaussel1(self):
         X = array([            # kasus j = k : solusi unik
@@ -28,7 +28,7 @@ class GaussElimTest(unittest.TestCase) :
             [10,11,12]
         ])
         print "kasus unik not full rank"
-        T = solve_linear( (X.astype(float)).T, Y) # input kolom perlu transp
+        T = solve_linear( (X.astype(float)).T, Y.astype(float) ) # input kolom perlu transp
         print T.T
         
     def test_gaussel2(self):
@@ -41,8 +41,68 @@ class GaussElimTest(unittest.TestCase) :
             [10, 11, 12]
         ])
         print "kasus unik not full rank"
-        T = solve_linear( (X.astype(float)).T, Y) # input kolom perlu transp
+        T = solve_linear( (X.astype(float)).T, Y.astype(float) ) # input kolom perlu transp
         print T
+
+    def test_gaussel3(self) :
+        X = array([            # kasus j = k : solusi unik
+            [0,-2,-2],
+            [1,4,5],
+            [-1,-1,-4]
+        ])
+        Y = array([
+            [3, 1, -2]
+        ])
+        print "kasus unik full rank"
+        T = solve_linear( (X.astype(float)).T, Y.astype(float) ) # input kolom perlu transp
+        print T
+
+    def test_gaussel4(self) :
+        X = array([            # kasus j = k : solusi unik
+            [1,1,1],
+            [1,2,2],
+            [1,2,3]
+        ])
+        Y = array([
+            [1, 1, 1]
+        ])
+        print "kasus unik full rank"
+        T = solve_linear( (X.astype(float)).T, Y.astype(float) ) # input kolom perlu transp
+        print T
+        
+    def test_gaussel5(self) :
+        X = array([            # kasus j = k : solusi unik
+            [2,-1,0],
+            [-1,2,-1],
+            [0,-1,1]
+        ])
+        Y = array([
+            [0, 0, 1]
+        ])
+        print "kasus unik full rank"
+        T = solve_linear( (X.astype(float)).T, Y.astype(float) ) # input kolom perlu transp
+        print T
+
+    def test_gaussel6(self) :
+        X = array([            # kasus j = k : solusi unik
+            [0,-1,-1],
+            [4,7,8],
+            [-3,-5,-6]
+        ])
+        Y = array([
+            [3, 4, 5]
+        ])
+        print "kasus unik full rank"
+        T = solve_linear( (X.astype(float)).T, Y.astype(float) ) # input kolom perlu transp
+        print T
+
+    def test_gaussel7(self) :
+        X = array( [[1,2,3]] )
+        Y = array( [[1,2,3]] )
+        print "kasus paling dasar malah ketinggalan"
+        T = solve_linear( (X.astype(float)).T, Y.astype(float) ) # input kolom perlu transp
+        print T
+        
 if __name__ == "__main__" :
     unittest.main()
 
